482fa414ba6c8a7a13d88c951a2480f496fec45b
s.turgut <semihkenan12@gmail.com>
2025-09-16 17:01:02 +0300

    hotfix

--- app/src/main/java/com/example/myapplication/Settings.kt
+++ app/src/main/java/com/example/myapplication/Settings.kt
@@ -4,12 +4,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 
 @Composable
-fun Settings(modifier: Modifier = Modifier) {
+fun Settings() {
     Column {
-        Box(){
+        Box{
         Text("settings")
         }
     }
