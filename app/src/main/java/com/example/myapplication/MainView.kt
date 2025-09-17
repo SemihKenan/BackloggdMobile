260210d88cf1bdb05dbccd9e68a7b60256d004e8
s.turgut <semihkenan12@gmail.com>
2025-09-17 15:37:22 +0300

    Added BottomNavBar instead of Drawer, Added Setting menu and start navigate

--- app/src/main/java/com/example/myapplication/MainView.kt
+++ app/src/main/java/com/example/myapplication/MainView.kt
@@ -4,51 +4,43 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.DividerDefaults
-import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.theme.AppbarRenk
 import com.example.myapplication.ui.theme.YaziRenk
-import kotlinx.coroutines.launch
 
-data class DrawerMenuItem(
+data class BottomNavItem(
     val title: String,
     val icon: ImageVector,
     val route: String
 )
 
-val drawerItems = listOf(
-    DrawerMenuItem("Home", Icons.Default.Home, "home_route"),
-    DrawerMenuItem("Profile", Icons.Default.Person, "profile_route"),
-    DrawerMenuItem("Settings", Icons.Default.Settings, "settings_route")
+val bottomNavItems = listOf(
+    BottomNavItem("Home", Icons.Default.Home, "home_route"),
+    BottomNavItem("Profile", Icons.Default.Person, "profile_route"),
+    BottomNavItem("Settings", Icons.Default.Settings, "settings_route")
 )
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -56,90 +48,101 @@ val drawerItems = listOf(
 fun MainView(
 ) {
     val navController = rememberNavController()
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
+    //val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+   // val scope = rememberCoroutineScope()
     val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = navBackStackEntry?.destination?.route
+    val currentRoute = navBackStackEntry?.destination
+    Scaffold(
+        modifier = Modifier.wrapContentSize(),
+        containerColor = Color.White,
+        topBar = {
+            TopAppBar(
+                {
+                    val currentItem = bottomNavItems.find { it.route == currentRoute?.route }
+                    Text(currentItem?.title ?: "Başlık Yok")
+                },
+                navigationIcon = {
+                    if (navController.previousBackStackEntry != null && bottomNavItems.none
+                        {it.route == currentRoute?.route})
+                    {
+                    IconButton(
 
-    ModalNavigationDrawer(
-        drawerState = drawerState,
-        drawerContent = {
-        ModalDrawerSheet {
-            Text("Menü")
-            HorizontalDivider(modifier = Modifier, thickness = 5.dp, color = DividerDefaults.color)
-            drawerItems.forEach { item ->
-                NavigationDrawerItem(
-                    label = { Text(item.title) },
-                    selected = currentRoute == item.route,
-                    onClick = {
-                        navController.navigate(item.route)
-                        scope.launch { drawerState.close() }
-                    },
-                    icon = { Icon(item.icon, contentDescription = item.title) },
-                    modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp)
+                        onClick = {navController.popBackStack() } ) {
+                        Icon(
+                            Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Geri"
+                        )}
+                    }
+                },
+                colors = TopAppBarDefaults
+                    .topAppBarColors(
+                        navigationIconContentColor = Color.Red,
+                        containerColor = AppbarRenk,
+                        titleContentColor = YaziRenk
+                    ),
+
+                modifier = Modifier
+            )
+        },
+        bottomBar = {
+            BottomAppBar {
+            bottomNavItems.forEach {screen ->
+                val selected =currentRoute?.route == screen.route || // Doğrudan rota eşleşmesi
+                        currentRoute?.parent?.route == screen.route
+                IconButton(onClick = {
+                    navController.navigate(screen.route){
+                        popUpTo(navController.graph.findStartDestination().id){
+                            saveState=true
+                        }
+                        launchSingleTop =true
+                        restoreState=true
+                    }
+                },
+                    modifier = Modifier.weight(1f, true)
 
                 )
-                HorizontalDivider(modifier = Modifier, thickness = 5.dp, color = DividerDefaults.color)
-            }
-        }
-    },
-        content = {
-            Scaffold(
-                modifier = Modifier.wrapContentSize(),
-                containerColor = Color.White,
-                topBar = {
-                    TopAppBar(
-                        {
-                            val currentItem=drawerItems.find { it.route == currentRoute }
-                            Text(currentItem?.title ?: "Başlık Yok")
-                        },
-                        navigationIcon = {
-                            IconButton(
-                                onClick = { scope.launch { drawerState.open() } }){
-                                Icon(
-                                    Icons.Default.Menu,
-                                    contentDescription = "Menüyü Aç"
-                                )
-                            }
-                        },
-                        colors = TopAppBarDefaults
-                            .topAppBarColors(
-                                containerColor = AppbarRenk,
-                                titleContentColor = YaziRenk
-                            ),
-
-                        modifier = Modifier
-                    )
-                },
-                content = {
-                        innerPadding ->
-                    Column(modifier = Modifier
-                        .padding(innerPadding)
-                        .wrapContentSize()) {
-                        NavHost(
-                            navController = navController,
-                            startDestination = "settings_route",
-                            contentAlignment = Alignment.Center,
-                            modifier = Modifier.wrapContentSize(),
+                {
+                    Column {
+                        Icon(
+                            imageVector = screen.icon,
+                            contentDescription = screen.title,
+                            tint = if (selected) Color.Red else Color.Gray
                         )
-                        {
-                            composable("game_route") {GamePage()}
-                            composable("settings_route") {Settings()}
-                            composable("home_route"){HomeScreen()}
-                            composable("profile_route"){Profil()}
-                            composable (route="settings_route"){Settings()}
+                        if (selected) {
+                            Text(text = screen.title)
                         }
                     }
 
-
                 }
-
-
-            )
+            }
         }
+                    },
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .wrapContentSize()
+            ) {
+                NavHost(
+                    navController = navController,
+                    startDestination = "home_route",
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.wrapContentSize(),
+                )
+                {
+                    composable("game_route") { GamePage() }
+                    composable("settings_route") { Settings(navController) }
+                    composable("home_route") { HomeScreen() }
+                    composable("profile_route") { Profil() }
+                }
+            }
+
+
+        }
+
+
     )
 }
-
 @Preview
 @Composable
 private fun MainviewPrev() {
--- app/src/main/java/com/example/myapplication/Settings.kt
+++ app/src/main/java/com/example/myapplication/Settings.kt
@@ -1,42 +1,61 @@
 package com.example.myapplication
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 
-val settingsTitles = listOf("Notifications", "Account","Privacy")
+val Titles = listOf("Notifications", "Account","Privacy")
 val settingTabs = listOf("Game Notifications", "Social Notifications", "Other Notifications")
-
+val accountTabs = listOf("Theme", "Account Info", "Language")
+val privacyTabs = listOf("Profile Privacy", "Change Password", "Others")
 @Composable
-fun Settings() {
+fun Settings(navController: NavController) {
     Scaffold(
-        content = { paddingValues -> Column(
-            Modifier
-                .padding(paddingValues)
-        )
-        {
-            Text("Bildirimler")
-            Box {
-                Column {
-                    Text("Oyun Bilirimleri")
-                    Text("Sosyal Bildirimler")
+        content = { paddingValues -> LazyColumn(
+            state = rememberLazyListState(),
+            modifier = Modifier.padding(paddingValues),
+            flingBehavior = ScrollableDefaults.flingBehavior(),
+            userScrollEnabled = true,
+            reverseLayout = false,
+            content = {
+                items(Titles.size)
+                {
+                    index->
+                    TextButton(
+                        onClick = {navController.navigate("game_route") }
+                    ) {
+                        Row {
+                            Text("Deneme")
+                        }
+                    }
+                    val currentTitle = Titles[index]
+                    Column { Text(currentTitle) }
+                    val tabstoShow = when (currentTitle) {
+                        "Notifications"-> settingTabs
+                        "Account" -> accountTabs
+                        "Privacy" -> privacyTabs
+                        else -> emptyList()
+                    }
+                    tabstoShow.forEach { tabTitle ->
+                        Text(tabTitle)
+                    }
+                    if (index < Titles.size - 1) {
+                        HorizontalDivider(modifier = Modifier.padding(top = 8.dp))
                     }
                 }
             }
-        }
+        )}
     )
-
-
-}
-
-@Preview
-@Composable
-private fun SettingsPrev() {
-    Settings()
 }
