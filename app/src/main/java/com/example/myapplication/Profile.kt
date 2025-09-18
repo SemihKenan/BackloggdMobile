8d13ffe8721252318ee62758bb1b6615a6fe7d6c
s.turgut <semihkenan12@gmail.com>
2025-09-18 16:55:02 +0300

    hotfix

--- app/src/main/java/com/example/myapplication/Profile.kt
+++ app/src/main/java/com/example/myapplication/Profile.kt
@@ -1,24 +1,24 @@
 package com.example.myapplication
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,108 +33,103 @@ val profileTitles= listOf("Recent Comment","Wish List","Favorites")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Profil() {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize(),
-        state = rememberLazyListState(),
-        reverseLayout = false,
-        verticalArrangement = Arrangement.spacedBy(20.dp),
-        flingBehavior = ScrollableDefaults.flingBehavior(),
-        userScrollEnabled = true
-    ) {
-        item {
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
+    Column(
+        modifier = Modifier.background(color = Color.White)){
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding()
+                .fillMaxWidth()
+                .background(color = Color.Black)
+        )
+
+        {
+            Box(
+                contentAlignment = Alignment.Center,
                 modifier = Modifier
-                    .padding()
-                    .fillMaxWidth()
-                    .background(color = Color.Black)
-            )
+                    .padding(7.dp)
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .background(Color.White)
+
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    tint = Color.DarkGray,
+                    modifier = Modifier.size(60.dp),
+                    contentDescription = "Profil Resmi",
+                )
+            }
+            Box(modifier = Modifier.size(100.dp), contentAlignment = Alignment.CenterEnd) {
+                Text(
+                    modifier = Modifier.padding(end = 7.dp),
+                    color = YaziRenk,
+                    text = "Oynanan Oyunlar:120"
+                )
+            }
+
+        }
+        LazyColumn(
+            Modifier,
+            state = rememberLazyListState(),
+            reverseLayout = false,
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            flingBehavior = ScrollableDefaults.flingBehavior(),
+            userScrollEnabled = false
+        )
             {
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier
-                        .padding(7.dp)
-                        .size(100.dp)
-                        .clip(CircleShape)
-                        .background(Color.White)
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Person,
-                        tint = Color.DarkGray,
-                        modifier = Modifier.size(60.dp),
-                        contentDescription = "Profil Resmi",
+                items(count = profileTitles.size)
+                {
+                    index ->Text(profileTitles[index])
+                    LazyRow(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        state = rememberLazyListState(),
+                        reverseLayout = false,
+                        horizontalArrangement = Arrangement.spacedBy(12.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        flingBehavior = ScrollableDefaults.flingBehavior(),
+                        userScrollEnabled =true
                     )
-                }
-                Box(modifier = Modifier.size(100.dp), contentAlignment = Alignment.CenterEnd) {
-                    Text(
-                        modifier = Modifier.padding(end = 7.dp),
-                        color = YaziRenk,
-                        text = "Yorumlananlar"
-                    )
+                    {
+                        items(10) { index ->
+                            Box(
+                                modifier = Modifier
+                                    .border(5.dp,Color.Black)
+                                    .size(height = 210.dp, width = 150.dp)
+                                    .background(color = Color.DarkGray)
+                            ) {
+                                Text(
+                                    "Game_Image $index",
+                                    modifier = Modifier
+                                        .padding(5.dp)
+                                        .align(Alignment.Center),
+                                    Color.White,
+                                    style = MaterialTheme.typography.bodyLarge
+                                )
+                                Text(
+                                    "Game Name $index",
+                                    Modifier
+                                        .align(Alignment.BottomStart)
+                                        .padding(10.dp),
+                                    Color.White
+                                )
+
+                            }
+                        }
+                    }
+
+
                 }
             }
-            Row { Box(Modifier.height(100.dp).padding(6.dp))}
-        }
     }
 }
 
 @Preview
 @Composable
-private fun prevprofile() {
+private fun ProfilePrev() {
     Profil()
+
 }
-// LazyColumn(
-//            Modifier,
-//            state = rememberLazyListState(),
-//            reverseLayout = false,
-//            verticalArrangement = Arrangement.spacedBy(10.dp),
-//            flingBehavior = ScrollableDefaults.flingBehavior(),
-//            userScrollEnabled = false
-//        )
-//            {
-//                items(count = profileTitles.size)
-//                {
-//                    index ->Text(profileTitles[index])
-//                    LazyRow(
-//                        Modifier
-//                            .fillMaxWidth()
-//                            .padding(8.dp),
-//                        state = rememberLazyListState(),
-//                        reverseLayout = false,
-//                        horizontalArrangement = Arrangement.spacedBy(12.dp),
-//                        verticalAlignment = Alignment.CenterVertically,
-//                        flingBehavior = ScrollableDefaults.flingBehavior(),
-//                        userScrollEnabled =true
-//                    )
-//                    {
-//                        items(10) { index ->
-//                            Box(
-//                                modifier = Modifier
-//                                    .border(5.dp,Color.Black)
-//                                    .size(height = 210.dp, width = 150.dp)
-//                                    .background(color = Color.DarkGray)
-//                            ) {
-//                                Text(
-//                                    "Game_Image $index",
-//                                    modifier = Modifier
-//                                        .padding(5.dp)
-//                                        .align(Alignment.Center),
-//                                    Color.White,
-//                                    style = MaterialTheme.typography.bodyLarge
-//                                )
-//                                Text(
-//                                    "Game Name $index",
-//                                    Modifier
-//                                        .align(Alignment.BottomStart)
-//                                        .padding(10.dp),
-//                                    Color.White
-//                                )
-//
-//                            }
-//                        }
-//                    }
-//
-//
-//                }
-//            }
--- app/src/main/java/com/example/myapplication/data/GameData.kt
+++ app/src/main/java/com/example/myapplication/data/GameData.kt
@@ -18,3 +18,24 @@ val mockGames=listOf(
     Game("9","Satisfactory",null,"Automation"),
     Game("10","Spider-Man",null,"Action"),
 )
+
+val AlreadyCommented=listOf(
+    Game("1","CyberPunk",null,"Action"),
+    Game("2","Batman Arkham Knight",null,"Action"),
+    Game("3","Zelda BOTW",null,"Action"),
+    Game("4","Hollow Knight",null,"Souls Like"),
+    Game("5","Prison Architect",null,"Base Building"),
+    Game("6","Satisfactory",null,"Automation"),
+    Game("7","Spider-Man",null,"Action"),
+)
+val Favorites=listOf(
+    Game("1","Batman Arkham Knight",null,"Action"),
+    Game("2","Zelda BOTW",null,"Action"),
+    Game("3","Hollow Knight",null,"Souls Like"),
+    Game("4","Prison Architect",null,"Base Building"),
+)
+val WishList=listOf(
+    Game("5","Shovel Knight",null,"Souls Like"),
+    Game("9","Satisfactory",null,"Automation"),
+)
+
